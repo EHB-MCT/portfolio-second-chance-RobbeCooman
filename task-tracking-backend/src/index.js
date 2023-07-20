f4c65b578ae5e8a9f0d82121df622617c3a071b4
RobbeCooman <robbe.cooman@student.ehb.be>
2023-07-20 11:15:48 +0200

    setup backend with database

--- task-tracking-backend/src/index.js
+++ task-tracking-backend/src/index.js
@@ -3,9 +3,15 @@ const cors = require('cors');
 const app = express();
 const db = require('./db');
 
+const tasksRouter = require('./routes/tasks');
+
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use('/api/tasks', tasksRouter);
+
 
 
 const PORT = process.env.PORT || 8000;

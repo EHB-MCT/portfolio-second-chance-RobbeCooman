e8ebb68588db6d001e17e84cae7e6a6bab04dd16
Robbe <robbe.cooman@student.ehb.be>
2023-08-21 22:08:47 +0200

    fix: database
    fixed database so it works with docker and add env file

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+const app = express();
+const db = require('./db/db');
 const router = express.Router();
-const db = require('../db');
+
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/podcasts', router);
+
+app.use(express.static('docs'));
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
 
 /**
  * @returns {Array} List of podcast objects
